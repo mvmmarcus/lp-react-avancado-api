9045bf63aa9eeaffe25edaa61e697056f8e1eb07
Marcus Vinicius Marques <marcus.vinicius.marques@hotmail.com>
2021-07-04 23:37:31 -0300

    feat: add database configs

--- config/server.js
+++ config/server.js
@@ -1,9 +1,3 @@
 module.exports = ({ env }) => ({
-  host: env('HOST', '0.0.0.0'),
-  port: env.int('PORT', 1337),
-  admin: {
-    auth: {
-      secret: env('ADMIN_JWT_SECRET', 'b769fffffbd5c32a32954c104b10bc9e'),
-    },
-  },
+  url: env("MY_HEROKU_URL"),
 });
